5df8ded006f1d0f1807b20a6918a14451b4a4b85
greg-krulin <grgr279@gmail.com>
2023-03-29 22:18:24 -0700

    Added save_csv function to fileio.py file.

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -28,3 +28,27 @@ def load_csv(csvpath):
         for row in csvreader:
             data.append(row)
     return data
+
+
+def save_csv(list_a, csvpath):
+    """
+    Saves a list to a CSV file from path provided. If path is not a csv.file, function will create one when
+    it starts to write.
+
+    Args:
+    list_a: A list of any objects
+    csvpath: A path for the CSV file which will have list written on it
+
+    Returns:
+    Doesn't return anything, just let user knows that csv file has been saved.
+    
+    
+    
+    """
+    with open(csvpath, 'w', newline = '') as csvfile:
+        csvwriter = csv.writer(csvfile)
+
+        for a in list_a:
+            csvwriter.writerow(a)
+
+    print("Finished saving to csv file.")
