880e30e267b8259acf958ea9324f15a4d0688f97
greg-krulin <grgr279@gmail.com>
2023-03-29 23:21:54 -0700

    Added comments to code and cleaned code a little for readability. Also added header for save_csv

--- qualifier/utils/fileio.py
+++ qualifier/utils/fileio.py
@@ -45,10 +45,20 @@ def save_csv(list_a, csvpath):
     
     
     """
+    #Uses function open from csv import to open the csv file, write on file, and let program know it is a csv file. 
+    #If the file does not exist it will be created.
     with open(csvpath, 'w', newline = '') as csvfile:
-        csvwriter = csv.writer(csvfile)
 
+        csvwriter = csv.writer(csvfile) #Calls writer function.
+
+        #Assigns header as a list of strings which will be our header for the csv file.
+        header = ['Lender', 'Max Loan Amount', 'Max LTV', 'Max DTI', 'Min Credit Score', 'Interest Rate']
+
+        #Writes header values before filling in values from qualifying loans.
+        csvwriter.writerow(header)
+
+        #Goes through list provider by user and writes each entry into csv file.
         for a in list_a:
             csvwriter.writerow(a)
 
-    print("Finished saving to csv file.")
+    print("Finished saving to a CSV.")
